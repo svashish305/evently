2230f574dfe46245bc1e13b241f08cdf5743321a
Shubhashish Verma <svashish305@gmail.com>
2024-01-31 00:48:14 +0530

    implement stripe

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,14 +1,14 @@
-'use server'
+"use server"
 
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from "next/cache"
 
-import { connectToDatabase } from '@/lib/database'
-import User from '@/lib/database/models/user.model'
-import Order from '@/lib/database/models/order.model'
-import Event from '@/lib/database/models/event.model'
-import { handleError } from '@/lib/utils'
+import { connectToDatabase } from "@/lib/database"
+import User from "@/lib/database/models/user.model"
+import Order from "@/lib/database/models/order.model"
+import Event from "@/lib/database/models/event.model"
+import { handleError } from "@/lib/utils"
 
-import { CreateUserParams, UpdateUserParams } from '@/types'
+import { CreateUserParams, UpdateUserParams } from "@/types"
 
 export async function createUser(user: CreateUserParams) {
   try {
