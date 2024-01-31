002b203d860084da988d6717af5dd9c32aef9185
Shubhashish Verma <svashish305@gmail.com>
2024-01-31 09:46:27 +0530

    fixed locale

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,11 +32,11 @@ export const formatDateTime = (dateString: Date) => {
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   }
 
-  const formattedDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions)
+  const formattedDateTime: string = new Date(dateString).toLocaleString('en-IN', dateTimeOptions)
 
-  const formattedDate: string = new Date(dateString).toLocaleString('en-US', dateOptions)
+  const formattedDate: string = new Date(dateString).toLocaleString('en-IN', dateOptions)
 
-  const formattedTime: string = new Date(dateString).toLocaleString('en-US', timeOptions)
+  const formattedTime: string = new Date(dateString).toLocaleString('en-IN', timeOptions)
 
   return {
     dateTime: formattedDateTime,
